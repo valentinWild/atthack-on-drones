432fcf1cdd7e76d213fb417e6c1625bae713b06d
Luise Donat <donatluise@gmail.com>
2024-08-07 19:36:29 +0200

    Added Liquid Shaders to all vials and a pour animation when tilting them

---
+++ Assets/PourEffect/Assets/Scripts/PourDetector.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PourDetector : MonoBehaviour
+{
+    public int pourThreshold = 45;
+    public Transform origin = null;
+    public GameObject streamPrefab = null; 
+
+    private bool isPouring = false;
+    private Stream currentStream = null;
+
+    private void Update()
+    {
+        bool pourCheck = CalculatePourAngle() < pourThreshold; 
+
+        if(isPouring != pourCheck)
+        {
+            isPouring = pourCheck;
+
+            if(isPouring)
+            {
+                StartPour();
+            }
+            else
+            {
+                StopPour();
+            }
+        }
+    }
+
+    private void StartPour()
+    {
+        print("Start");
+        currentStream = CreateStream();
+        currentStream.Begin();
+    }
+
+    private void StopPour()
+    {
+        print("End");
+        currentStream.End();
+        currentStream = null;
+    }
+
+    private float CalculatePourAngle()
+    {
+        return transform.forward.y * Mathf.Rad2Deg;
+    }
+
+    private Stream CreateStream() 
+    { 
+        GameObject streamObject = Instantiate(streamPrefab, origin.position, Quaternion.identity, transform);
+        return streamObject.GetComponent<Stream>(); 
+    }  
+}
