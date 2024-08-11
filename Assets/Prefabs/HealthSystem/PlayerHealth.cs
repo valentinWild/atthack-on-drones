1266067ad5c094c00274f9356784549c21b50a0e
labipetra <117845863+labipetra@users.noreply.github.com>
2024-08-11 14:50:25 +0200

    added description/comments

--- Assets/Prefabs/HealthSystem/PlayerHealth.cs
+++ Assets/Prefabs/HealthSystem/PlayerHealth.cs
@@ -5,12 +5,12 @@ using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-    private float health;
-    private float lerpTimer;
-    public float maxHealth = 100;
-    public float chipSpeed = 2;
-    public Image frontHealthBar;
-    public Image backHealthBar;
+    private float health; //current health of the player
+    private float lerpTimer; // timer used for controlling the smooth transition of the health bar
+    public float maxHealth = 100; //the maximum health the player can have
+    public float chipSpeed = 2; //the speed at which the health bar transition occurs when health changes.
+    public Image frontHealthBar; //UI image representing the current health, blue one
+    public Image backHealthBar; //UI image, grey one, changing color depending on damage/restore
 
     // Start is called before the first frame update
     void Start()
@@ -21,10 +21,11 @@ public class PlayerHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        health = Mathf.Clamp(health, 0, maxHealth);
+        //the health value stays within the range of 0 to maxHealth
+        health = Mathf.Clamp(health, 0, maxHealth); 
         UpdateHealthUI();
 
-        // Überprüfe, ob die Leertaste (Space) gedrückt wurde
+        
         if (Input.GetKeyDown(KeyCode.Space))
         {
             TakeDamage(Random.Range(5, 10));
@@ -39,11 +40,16 @@ public class PlayerHealth : MonoBehaviour
 
     public void UpdateHealthUI()
     {
+        //Store the current fill amounts of the front and back health bars
         Debug.Log(health);
-        float fillF = frontHealthBar.fillAmount;
+        float fillF = frontHealthBar.fillAmount; 
         float fillB = backHealthBar.fillAmount;
+        //calculates the fraction of health relative to the maximum health, this fraction is used to determine how full the health bars should be
         float hFraction = health / maxHealth;
 
+
+        //wenn der hintere Balken größer ist als der vordere, zeigt das, dass der Spieler gerade Leben verloren hat
+        //der hintere Balken beginnt dann langsam zu schrumpfen und gleicht sich dem vorderen Balken an
         if (fillB > hFraction)
         {
             frontHealthBar.fillAmount = hFraction;
@@ -54,6 +60,8 @@ public class PlayerHealth : MonoBehaviour
             backHealthBar.fillAmount = Mathf.Lerp(fillB, hFraction, percentComplate);
         }
 
+        //der hintere Balkon reagiert auf die Heilung und springt direkt zur neuen höheren Gesundheit
+        //der vordere Balkon fängt an langsam zu wachsen
         if (fillF < hFraction)
         {
             backHealthBar.color = Color.green;
