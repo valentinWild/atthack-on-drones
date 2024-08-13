61f951b3880bb0270899f3a47bcf83d1fea1231a
labipetra <117845863+labipetra@users.noreply.github.com>
2024-08-13 12:35:30 +0200

    added HealthSystem

---
+++ Assets/Prefabs/HealthSystem/PlayerHealth.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerHealth : MonoBehaviour
+{
+    private float health; //current health of the player
+    private float lerpTimer; // timer used for controlling the smooth transition of the health bar
+    public float maxHealth = 100; //the maximum health the player can have
+    public float chipSpeed = 2; //the speed at which the health bar transition occurs when health changes.
+    public Image frontHealthBar; //UI image representing the current health, blue one
+    public Image backHealthBar; //UI image, grey one, changing color depending on damage/restore
+    public TextMeshProUGUI healthText;
+    //public TextMeshProUGUI friendCounterText;
+    //public TextMeshProUGUI enemyCounterText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //the health value stays within the range of 0 to maxHealth
+        health = Mathf.Clamp(health, 0, maxHealth); 
+        UpdateHealthUI();
+
+        //UpdateFriendCounter(DroneCounter.GetCollectedCounter());
+        //UpdateEnemyCounter(DroneCounter.GetExplosionCounter());
+
+       /* if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(Random.Range(5, 10));
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            RestoreHealth(Random.Range(5, 10));
+        }*/
+
+    }
+
+    public void UpdateHealthUI()
+    {
+        //Store the current fill amounts of the front and back health bars
+        Debug.Log(health);
+        float fillF = frontHealthBar.fillAmount; 
+        float fillB = backHealthBar.fillAmount;
+        //calculates the fraction of health relative to the maximum health, this fraction is used to determine how full the health bars should be
+        float hFraction = health / maxHealth;
+
+
+        //wenn der hintere Balken größer ist als der vordere, zeigt das, dass der Spieler gerade Leben verloren hat
+        //der hintere Balken beginnt dann langsam zu schrumpfen und gleicht sich dem vorderen Balken an
+        if (fillB > hFraction)
+        {
+            frontHealthBar.fillAmount = hFraction;
+            backHealthBar.color = Color.red;
+            lerpTimer += Time.deltaTime;
+            float percentComplate = lerpTimer / chipSpeed;
+            percentComplate = percentComplate * percentComplate;
+            backHealthBar.fillAmount = Mathf.Lerp(fillB, hFraction, percentComplate);
+        }
+
+        //der hintere Balkon reagiert auf die Heilung und springt direkt zur neuen höheren Gesundheit
+        //der vordere Balkon fängt an langsam zu wachsen
+        if (fillF < hFraction)
+        {
+            backHealthBar.color = Color.green;
+            backHealthBar.fillAmount = hFraction;
+            lerpTimer += Time.deltaTime;
+            float percentComplate = lerpTimer / chipSpeed;
+            percentComplate = percentComplate * percentComplate;
+            frontHealthBar.fillAmount = Mathf.Lerp(fillF, backHealthBar.fillAmount, percentComplate);
+        }
+
+        healthText.text = Mathf.Round(health) + "/" + Mathf.Round(maxHealth);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        lerpTimer = 0f;
+    }
+
+    public void RestoreHealth(float healAmount)
+    {
+        health += healAmount;
+        lerpTimer = 0f;
+    }
+
+    public void IncreaseHealth(int level)
+    {
+        maxHealth += (health * 0.01f)* ((100 - level) * 0.1f);
+        health = maxHealth;
+    }
+
+    /*public void UpdateFriendCounter(int friendCount)
+    {
+        friendCounterText.text = friendCount.ToString();
+    }
+
+    public void UpdateEnemyCounter(int enemyCount)
+    {
+        enemyCounterText.text = enemyCount.ToString();
+    }*/
+}
--- Assets/Scripts/Runner/PlayerCollision.cs
+++ Assets/Scripts/Runner/PlayerCollision.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class PlayerCollision : MonoBehaviour
 {
     [SerializeField] private string bulletTag = "Bullet";// Tag für Drone Shots
+    [SerializeField] private float damageAmount = 2f; // Schaden, den der Spieler erleidet
+    private PlayerHealth playerHealth; // Referenz auf das PlayerHealth-Skript
+
+    private void Start()
+    {
+        // Sucht nach dem PlayerHealth-Skript im selben GameObject
+        playerHealth = GetComponent<PlayerHealth>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -13,6 +21,12 @@ public class PlayerCollision : MonoBehaviour
         {
             // Nachricht ausgeben, wenn der Spieler getroffen wird
             Debug.Log("User is shooted by drones");
+
+            // Schaden dem Spieler zufügen
+            if (playerHealth != null)
+            {
+                playerHealth.TakeDamage(damageAmount);
+            }
 
             //Zerstören des Bullet-Objekts
             Destroy(other.gameObject, 0.1f);
