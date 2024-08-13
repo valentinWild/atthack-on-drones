7df6c317b215c3a38e8c6f08d236dfc0ebdce83f
scpxiao <64504003+scpxiao@users.noreply.github.com>
2024-08-13 19:41:46 +0200

    fixed real time text binding to inspector value
     Hiint Counter was missing OnValidate() function

---
+++ Assets/Prefabs/Lab/UI/HintCounter.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HintCounter : MonoBehaviour
+{
+    [SerializeField] private int _hintCounter = 0;
+    private TextMeshProUGUI _text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _text = GetComponent<TextMeshProUGUI>();
+        UpdateText();  // Initial update
+    }
+
+    public void IncreaseCounter()
+    {
+        _hintCounter++;
+        Debug.Log("Increased Hint Counter to: " + _hintCounter);
+        UpdateText();
+    }
+
+    private void UpdateText()
+    {
+        if (_text != null)
+        {
+            _text.text = _hintCounter.ToString();
+        }
+    }
+
+    public void ResetCounter()
+    {
+        _hintCounter = 0;
+        UpdateText();
+    }
+
+    void OnValidate()
+    {
+        if (Application.isPlaying)
+        {
+            UpdateText();
+        }
+    }
+}
