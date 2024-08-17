b65c9343621dcda9d3b032664ef8d8c59ff854a4
Luise Donat <donatluise@gmail.com>
2024-08-17 16:48:40 +0200

    small changes

--- Assets/Scripts/Lab/MixingVial.cs
+++ Assets/Scripts/Lab/MixingVial.cs
@@ -211,7 +211,7 @@ public class MixingVial : MonoBehaviour
             Debug.Log("Failed to mix the liquids");
             fail.Play(); // Play failure sound
             potionCreated = false;
-            ShowPotionMessage("Unvalid combination");
+            ShowPotionMessage("Invalid combination");
             StartCoroutine(ResetPotion());
         }
 
