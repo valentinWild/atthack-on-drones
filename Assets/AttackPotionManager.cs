39c65e8abf47f0e1563bdbfb9dfd4280e50643eb
labipetra <117845863+labipetra@users.noreply.github.com>
2024-08-18 16:11:39 +0200

    Added Attack Potion visual and sound effect

---
+++ Assets/AttackPotionManager.cs
@@ -0,0 +1,88 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Rendering.PostProcessing;
+
+public class AttackPotionManager : MonoBehaviour
+{
+    public float intensity = 0;
+
+    PostProcessVolume _volume;
+    Vignette _vignette;
+
+    private void OnEnable()
+    {
+        if (GameSyncManager.Instance)
+        {
+            GameSyncManager.OnActivePotionChanged += OnActivePotionChanged;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (GameSyncManager.Instance)
+        {
+            GameSyncManager.OnActivePotionChanged -= OnActivePotionChanged;
+        }
+    }
+
+    void Start()
+    {
+        _volume = GetComponent<PostProcessVolume>();
+        _volume.profile.TryGetSettings<Vignette>(out _vignette);
+
+        if (!_vignette)
+        {
+            print("error, vignette is empty");
+        }
+
+        else
+        {
+            _vignette.enabled.Override(false);
+        }
+    }
+
+    private void OnActivePotionChanged(string potionType)
+    {
+        if (potionType == "Attack Potion")
+        {
+            AudioSource audioSource = GetComponent<AudioSource>();
+            if (audioSource != null && audioSource.clip != null)
+            {
+                audioSource.Play();
+            }
+            else
+            {
+                Debug.LogWarning("AudioSource or AudioClip is missing!");
+            }
+
+            StartCoroutine(TakeAttackPotionEffect());
+        }
+    }
+
+    public IEnumerator TakeAttackPotionEffect()
+    {
+        intensity = 0.4f;
+
+        _vignette.enabled.Override(true);
+        _vignette.intensity.Override(intensity);
+
+        yield return new WaitForSeconds(5.0f);
+
+        while (intensity > 0)
+        {
+            intensity -= 0.01f;
+
+            if (intensity < 0) intensity = 0;
+
+            _vignette.intensity.Override(intensity);
+
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        _vignette.enabled.Override(false);
+        yield break;
+
+    }
+}
